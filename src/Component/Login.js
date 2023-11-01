13bf8e524c2f695923c822be652d43a6c2e6eb34
sandeep <sandipsangles819@gmail.com>
2023-11-01 13:11:01 +0530

    Resolving Routing issue

--- src/Component/Login.js
+++ src/Component/Login.js
@@ -119,12 +119,11 @@ const Login = () => {
                   </button>
                   <p className="text-sm font-semibold mt-2 pt-1 mb-0">
                     Don't have an account?
-                    <a
-                      href="/"
+                    <Link to ="/register"
                       className="text-red-600 hover:text-red-700 focus:text-red-700 transition duration-200 ease-in-out"
                     >
                       Register
-                    </a>
+                    </Link>
                   </p>
                 </div>
               </form>
--- src/Component/Registration.js
+++ src/Component/Registration.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Login from "./Login";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import { useEffect } from "react";
 import Navbar from "../Navbar";
 const Registration = () => {
@@ -93,14 +93,14 @@ const Registration = () => {
                       className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
                       data-mdb-ripple="true"
                       data-mdb-ripple-color="light"
+                      
                     >
                       Register
                     </button>
                     <div className="flex items-center my-4 before:flex-1 before:border-t before:border-gray-300 before:mt-0.5 after:flex-1 after:border-t after:border-gray-300 after:mt-0.5">
                       <p className="text-center font-semibold mx-4 mb-0">OR</p>
                     </div>
-                    <a
-                    href='/login'> Already Registered {name}, login in?</a>
+                    <Link to ='/login'> Already Registered {name}, login in?</Link>
                   </form>
                 </div>
               </div>
